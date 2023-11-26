5d91435f5e252cd91b2505564ec7a6477e90a24f
Tejas Dixit <tejasdixit17@gmail.com>
2023-11-27 02:26:06 +0530

    added about view

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
 # Create your views here.
+
+def home(request):
+    """This is home page route"""
+    return HttpResponse('<h1> Blog Home page </h1>')
+
+def about(request):
+    """This is about us route"""
+    return HttpResponse('<h1> About Us </h1>')
+
